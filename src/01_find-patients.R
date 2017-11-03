1b956275199ecadeacbd80da375fa1dbd874639f
bgulbis <brian.gulbis@gmail.com>
2017-11-03 09:56:30 -0500

    save list of patients receiving dose

--- src/01_find-patients.R
+++ src/01_find-patients.R
@@ -34,5 +34,22 @@ mbo_med <- concat_encounters(pts_med$millennium.id)
 #   * Medications - Inpatient - Prompt
 #       - Medication (Generic): prothrombin complex;prothrombin complex human
 
+meds <- read_data(dir_raw, "meds-inpt", FALSE) %>%
+    as.meds_inpt() %>%
+    filter(med.location %in% c("HH EDTR", "HH EREV", "HH VUHH", "HH EDHH"))
+
+mbo_id <- concat_encounters(unique(meds$millennium.id))
+
 # run MBO query
-#   * Identifiers
+#   * Identifiers - by Millennium Encounter Id
+
+meds_id <- read_data(dir_raw, "id", FALSE) %>%
+    distinct() %>%
+    rename(millennium.id = `Encounter Identifier`,
+           fin = `Financial Number`)
+
+meds %>%
+    left_join(meds_id, by = "millennium.id") %>%
+    select(fin, med.datetime, med, med.dose, med.dose.units, route, med.location) %>%
+    arrange(fin, med.datetime) %>%
+    write.csv("data/external/kcentra_patients.csv", row.names = FALSE)
