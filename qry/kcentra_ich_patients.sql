27d6a0b5c9df102b2d16caf098992cc02a7d7162
Brian Gulbis <brian.gulbis@gmail.com>
2019-09-06 13:48:33 -0500

    use fixed dates in query

--- qry/kcentra_ich_patients.sql
+++ qry/kcentra_ich_patients.sql
@@ -1,6 +1,8 @@
 SELECT DISTINCT
+	ENCOUNTER.ENCNTR_ID AS ENCOUNTER_ID,
 	ENCNTR_ALIAS.ALIAS AS FIN,
-	MAX(CE_INR.RESULT_VAL) KEEP (DENSE_RANK FIRST ORDER BY CE_INR.EVENT_END_DT_TM) AS INR_MAX
+	VALIDATE_CONVERSION(CE_INR.RESULT_VAL AS NUMBER) AS NUM_RESULT
+	MAX(TO_NUMBER(CE_INR.RESULT_VAL DEFAULT 0 ON CONVERSION ERROR)) KEEP (DENSE_RANK FIRST ORDER BY CE_INR.EVENT_END_DT_TM) AS INR_MAX
 FROM
 	CLINICAL_EVENT,
 	CLINICAL_EVENT CE_INR,
@@ -9,12 +11,14 @@ FROM
 	ENCOUNTER,
 	NOMENCLATURE
 WHERE
+	-- prothrombin complex
 	CLINICAL_EVENT.EVENT_CD = 926562948
+	AND CLINICAL_EVENT.VALID_UNTIL_DT_TM > DATE '2099-12-31' 
+	AND CLINICAL_EVENT.EVENT_END_DT_TM BETWEEN DATE '2015-09-01' AND DATE '2019-07-01'
 	AND (
 		CLINICAL_EVENT.ENCNTR_ID = ENCOUNTER.ENCNTR_ID
-		AND CLINICAL_EVENT.PERSON_ID = ENCOUNTER.PERSON_ID
 		AND ENCOUNTER.ACTIVE_IND = 1
-		AND ENCOUNTER.LOC_FACILITY_CD IN (3310, 3796, 3821, 3822, 3823)
+		AND ENCOUNTER.LOC_FACILITY_CD IN (3310, 3796)
 	)
 	AND (
 		ENCOUNTER.ENCNTR_ID = ENCNTR_ALIAS.ENCNTR_ID
@@ -30,79 +34,58 @@ WHERE
     AND (
         DIAGNOSIS.NOMENCLATURE_ID = NOMENCLATURE.NOMENCLATURE_ID
         AND NOMENCLATURE.ACTIVE_IND = 1
-        AND REGEXP_INSTR(NOMENCLATURE.SOURCE_IDENTIFIER, 'I61|I62|431|432.9') > 0
+        AND REGEXP_INSTR(NOMENCLATURE.SOURCE_IDENTIFIER, 'I61|I62') > 0
     )
     AND (
         CLINICAL_EVENT.ENCNTR_ID = CE_INR.ENCNTR_ID(+)
         AND CE_INR.EVENT_CD(+) = 32089
     )
-	AND (
-		CLINICAL_EVENT.EVENT_END_DT_TM + 0
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - 1, pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE - 7, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE - 1, 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {'0'}, User:2080), pi_time_zone(2, @Variable('BOUSER')))
-			)
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', pi_to_gmt(TRUNC(SYSDATE) + (86399 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Week to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Week', pi_to_gmt(TRUNC(SYSDATE, 'DAY') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Last Month', pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'Month to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
-				'User-defined', pi_to_gmt(
-					TO_DATE(
-						@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-					),
-					pi_time_zone(1, @Variable('BOUSER'))),
-				'N Days Prior', pi_to_gmt(SYSDATE, pi_time_zone(2, @Variable('BOUSER')))
-			)
-		AND CLINICAL_EVENT.EVENT_END_DT_TM
-			BETWEEN DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE),
-				'Yesterday', TRUNC(SYSDATE) - 1,
-				'Week to Date', TRUNC(SYSDATE, 'DAY'),
-				'Last Week', TRUNC(SYSDATE - 7, 'DAY'),
-				'Last Month', TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'),
-				'Month to Date', TRUNC(SYSDATE - 1, 'MONTH'),
-				'User-defined', DECODE(
-					@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter begin date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {0}, User:2080)
-			) - 1
-			AND DECODE(
-				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
-				'Today', TRUNC(SYSDATE) + (86399 / 86400),
-				'Yesterday', TRUNC(SYSDATE) - (1 / 86400),
-				'Week to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'Last Week', TRUNC(SYSDATE, 'DAY') - (1 / 86400),
-				'Last Month', TRUNC(SYSDATE, 'MONTH') - (1 / 86400),
-				'Month to Date', TRUNC(SYSDATE) - (1 / 86400),
-				'User-defined', DECODE(
-					@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
-					'01/01/1800 00:00:00',
-					'',
-					@Variable('Enter end date (Leave as 01/01/1800 if using a Relative Date)')
-				),
-				'N Days Prior', SYSDATE
-			) + 1
-	)
 GROUP BY
-    ENCNTR_ALIAS.ALIAS
+    ENCOUNTER.ENCNTR_ID,
+	ENCNTR_ALIAS.ALIAS
+
+UNION
+
+SELECT DISTINCT
+	ENCOUNTER.ENCNTR_ID AS ENCOUNTER_ID,
+	ENCNTR_ALIAS.ALIAS AS FIN,
+	MAX(CE_INR.RESULT_VAL) KEEP (DENSE_RANK FIRST ORDER BY CE_INR.EVENT_END_DT_TM) AS INR_MAX
+FROM
+	CLINICAL_EVENT,
+	CLINICAL_EVENT CE_INR,
+    DIAGNOSIS,
+	ENCNTR_ALIAS,
+	ENCOUNTER,
+	NOMENCLATURE
+WHERE
+	CLINICAL_EVENT.EVENT_CD = 926562948
+	AND CLINICAL_EVENT.VALID_UNTIL_DT_TM > DATE '2099-12-31' 
+	AND CLINICAL_EVENT.EVENT_END_DT_TM BETWEEN DATE '2013-05-01' AND DATE '2015-11-01'
+	AND (
+		CLINICAL_EVENT.ENCNTR_ID = ENCOUNTER.ENCNTR_ID
+		AND ENCOUNTER.ACTIVE_IND = 1
+		AND ENCOUNTER.LOC_FACILITY_CD IN (3310, 3796)
+	)
+	AND (
+		ENCOUNTER.ENCNTR_ID = ENCNTR_ALIAS.ENCNTR_ID
+		AND ENCNTR_ALIAS.ACTIVE_IND = 1
+		AND ENCNTR_ALIAS.END_EFFECTIVE_DT_TM > SYSDATE
+		AND ENCNTR_ALIAS.ENCNTR_ALIAS_TYPE_CD = 619
+	)
+	AND (
+	    CLINICAL_EVENT.ENCNTR_ID = DIAGNOSIS.ENCNTR_ID
+        AND DIAGNOSIS.ACTIVE_IND = 1
+        AND DIAGNOSIS.DIAG_TYPE_CD = 26244
+	)
+    AND (
+        DIAGNOSIS.NOMENCLATURE_ID = NOMENCLATURE.NOMENCLATURE_ID
+        AND NOMENCLATURE.ACTIVE_IND = 1
+        AND REGEXP_INSTR(NOMENCLATURE.SOURCE_IDENTIFIER, '431|432.9') > 0
+    )
+    AND (
+        CLINICAL_EVENT.ENCNTR_ID = CE_INR.ENCNTR_ID(+)
+        AND CE_INR.EVENT_CD(+) = 32089
+    )
+GROUP BY
+    ENCOUNTER.ENCNTR_ID,
+	ENCNTR_ALIAS.ALIAS
